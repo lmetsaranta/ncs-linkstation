e98139898fd79751a4840dbe618ac98e9295e1c1
Lennu Metsaranta <lennu.metsaranta@solibri.com>
2021-08-20 13:10:43 +0300

    Writed tests with jest
    Jest script added to package.json
    Add node modules to gitignore
    Show power with only two decimals

--- src/assets/linkstations.js
+++ src/assets/linkstations.js
@@ -2,6 +2,6 @@ let Linkstation = require("../linkstation");
 
 exports.stationArray = [
   new Linkstation(0, 0,10),
-  new Linkstation(10, 10,5),
+  new Linkstation(20, 20,5),
   new Linkstation(10, 0,12)
 ];
--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 let linkstations = require("./assets/linkstations");
 let devices = require("./assets/devices");
 
-// pythagoras theorem a² + b² = c²
+/** pythagoras theorem a² + b² = c²
+ *
+ * @param {number} x1 X-coordinate of point 1
+ * @param {number} y1 Y-coordinate of point 1
+ * @param {number} x2 X-coordinate of point 2
+ * @param {number} y2 Y-coordinate of point 2
+ * @returns {number} Distance between two points
+ */
 const distanceBetweenTwoPoints = (x1, y1, x2, y2) => {
   let a = x1 - x2;
   let b = y1 - y2;
@@ -13,6 +20,7 @@ const distanceBetweenTwoPoints = (x1, y1, x2, y2) => {
  *  power = (reach - device's distance from linkstation)²
  *  @param {Object} device Device object with x & y coordinates
  * @param {Object} linkstation Linkstation object with x & y coordinates and reach of the station
+ * @returns {number} Power of the best linkstation or 0 if no linkstation within reach
  */
 const power = (device, linkstation) => {
   let dist = distanceBetweenTwoPoints(device.x, device.y, linkstation.x, linkstation.y);
@@ -29,6 +37,7 @@ const power = (device, linkstation) => {
  *  Main function to calculate best linkstation for the device
  *  @param {Object.<stationArray[]>} linkstations Array of linkstations available
  *  @param {Object} device Device object with x & y coordinates
+ *  @returns {string} Output string of reachable station or no station within reach
  */
 const main = (linkstations, device) => {
   let pow = 0;
@@ -36,20 +45,22 @@ const main = (linkstations, device) => {
 
   linkstations.stationArray.map((station) => {
     let p = power(device, station);
-      if(p > 0) {
+      if(p > pow) {
         pow = p;
         bestStation = station;
       }
   });
 
   if(pow === 0) {
-    console.log(`No link station within reach for point ${device.x},${device.y}`);
+    return `No link station within reach for point ${device.x},${device.y}`;
   } else {
-    console.log(`Best link station for point ${device.x},${device.y} is ${bestStation.x},${bestStation.y} with power ${pow}`);
+    return `Best link station for point ${device.x},${device.y} is ${bestStation.x},${bestStation.y} with power ${pow.toFixed(2)}`;
   }
 };
 
 // Run example output for predefined devices
 devices.deviceArray.map((device) => {
-  main(linkstations, device);
+  console.log(main(linkstations, device));
 });
+
+module.exports = main;
